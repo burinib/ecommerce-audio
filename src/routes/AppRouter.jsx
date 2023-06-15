e6fc19a580ebc5a44933892ab1b7c4c0fd037e02
Braian Burini <braianb55@hotmail.com>
2023-06-15 17:44:31 -0300

    creando rutas

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { menuRoutes } from "./routes";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      {menuRoutes.map(({ id, path, Element }) => (
+        <Route key={id} path={path} element={<Element />} />
+      ))}
+      <Route path="*" element={<h1>Not found</h1>} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
