42e1f2012d1cce30bbc718925323ab8c4e82d56d
Braian Burini <braianb55@hotmail.com>
2023-06-20 16:52:34 -0300

    styles navbar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -3,12 +3,21 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
+import "./Navbar.css";
+
 const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+        <Toolbar
+          sx={{
+            backgroundColor: "#0E0E0E",
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{ display: { xs: "flex", md: "none" }, alignItems: "center" }}
+          >
             <IconButton
               size="large"
               edge="start"
@@ -25,18 +34,27 @@ const Navbar = () => {
             <ShoppingCartIcon />
           </Box>
 
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex", backgroundColor: "#0E0E0E" },
+              justifyContent: "center",
+              width: "100%",
+              alignItems: "center",
+            }}
+          >
             <img
               src="https://res.cloudinary.com/dwqrlr45w/image/upload/v1682637939/audiophileEcommerce/shared/desktop/logo_qnvapf.svg"
               alt=""
             />
-            <div>
-              <Link>HOME</Link>
-              <Link>HEADPHONES</Link>
-              <Link>SPEAKERS</Link>
-              <Link>EARPHONES</Link>
+            <div className="containerLinks">
+              <Link className="links">HOME</Link>
+              <Link className="links">HEADPHONES</Link>
+              <Link className="links">SPEAKERS</Link>
+              <Link className="links">EARPHONES</Link>
             </div>
-            <ShoppingCartIcon />
+            <Box sx={{ marginLeft: "auto" }}>
+              <ShoppingCartIcon className="carritoIcon" />
+            </Box>
           </Box>
         </Toolbar>
       </AppBar>
@@ -45,4 +63,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-Navbar;
