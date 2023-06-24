1a3239dab2ec6fb06d48474a1964848ab1677bb0
Braian Burini <braianb55@hotmail.com>
2023-06-24 16:22:29 -0300

    work

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,45 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import "./Home.css";
+import { BotonNaranja } from "../../common/custom/customComponent";
 
 const Home = () => {
   return (
-    <Box>
-      <div className="box-container"></div>
+    <Box className="container">
+      <Box className="box-container"></Box>
+      <Box className="content-container" sx={{ justifyContent: "flex-start" }}>
+        <Typography
+          sx={{
+            color: "white",
+            marginBottom: "20px",
+          }}
+        >
+          NEW PRODUCT
+        </Typography>
+        <Typography
+          sx={{
+            color: "white",
+            marginBottom: "20px",
+            fontFamily: "Roboto",
+            textTransform: "uppercase",
+            fontSize: "40px",
+          }}
+        >
+          XX99 Mark II Headphones
+        </Typography>
+        <Typography
+          sx={{
+            color: "white",
+            marginBottom: "20px",
+            fontFamily: "Roboto",
+          }}
+        >
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </Typography>
+        <Box sx={{ marginTop: "auto", alignSelf: "flex-start" }}>
+          <BotonNaranja variant="contained">See Product</BotonNaranja>
+        </Box>
+      </Box>
     </Box>
   );
 };
