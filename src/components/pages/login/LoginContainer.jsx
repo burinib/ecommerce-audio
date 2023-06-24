e1f9564b4afd7caf508f50c74755fd4549a62f9d
Braian Burini <braianb55@hotmail.com>
2023-06-24 16:17:43 -0300

    woek login

--- src/components/pages/login/LoginContainer.jsx
+++ src/components/pages/login/LoginContainer.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react";
 import Login from "./Login";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const LoginContainer = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShow = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const { handleSubmit, handleChange, errors } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    onSubmit: (data) => {
+      console.log("Se envio", data);
+      //conexion con firebase
+    },
+    validateOnChange: false,
+    validationSchema: Yup.object({
+      email: Yup.string("deben ser caracteres")
+        .email("no corresponde a un mail valido")
+        .required("Este campo es obligatorio"),
+      password: Yup.string("deben ser caracteres").required(
+        "Este campo es obligatorio"
+      ),
+    }),
+  });
+
   return (
     <div>
-      <Login />
+      <Login
+        showPassword={showPassword}
+        handleShow={handleShow}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        errors={errors}
+      />
     </div>
   );
 };
